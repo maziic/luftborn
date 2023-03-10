4348c2b595dc18ff69f1c0b37bd195c2ad99f893
maged mohamed <maged0mohamed@outlook.com>
2023-03-10 17:23:02 +0200

    Add project files.

---
+++ luftborn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace luftborn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
